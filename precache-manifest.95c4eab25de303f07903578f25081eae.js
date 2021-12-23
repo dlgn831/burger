5f39c2246cbf8a096d2788f4bda4f5f244718085
Dulguun <Dulguun.otgonbayar831@gmail.com>
2021-12-23 13:54:22 +0800

    Updates

---
+++ precache-manifest.95c4eab25de303f07903578f25081eae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482ff7ad61ab954567bb1c16cc41210a",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "c042444c21b91774e799",
+    "url": "/burger/static/css/main.84045e09.chunk.css"
+  },
+  {
+    "revision": "ec0686b586d9910cf438",
+    "url": "/burger/static/js/2.925703ed.chunk.js"
+  },
+  {
+    "revision": "e540cb51dc4f24037f1158e0238ef6f2",
+    "url": "/burger/static/js/2.925703ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c042444c21b91774e799",
+    "url": "/burger/static/js/main.b0789386.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
